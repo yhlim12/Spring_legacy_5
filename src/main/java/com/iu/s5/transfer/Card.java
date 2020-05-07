969990ba71cd544f81d8043c043d7041a9e5a859
yhlim12 <63095268+yhlim12@users.noreply.github.com>
2020-05-07 12:38:29 +0900

    aoc commit

--- src/main/java/com/iu/s5/transfer/Card.java
+++ src/main/java/com/iu/s5/transfer/Card.java
@@ -1,14 +1,21 @@
 package com.iu.s5.transfer;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class Card {
-	
+	@AfterReturning("execution(* com.iu.s5.transfer.Transfer.*())")
 	public void cardAfter() {
 		System.out.println("-----------------------------");
 		System.out.println("카드 결제");
 	}
 
+	@Around("execution(* com.iu.s5.transfer.Transfer.*(Integer,..))")
 	public Object cardCheck(ProceedingJoinPoint join)throws Throwable{
 		System.out.println("----------------------------------");
 		System.out.println("카드 Check in");
--- src/main/java/com/iu/s5/transfer/Transfer.java
+++ src/main/java/com/iu/s5/transfer/Transfer.java
@@ -1,15 +1,18 @@
 package com.iu.s5.transfer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Transfer {
 	
-	public void bus(int money) {
+	public void bus(Integer money) {
 		System.out.println("============================");
 		System.out.println("Bus 타기");
 		System.out.println("핸드폰 보기");
 		System.out.println("============================");	
 	}
 	
-	public void subway(int money, String line) {
+	public void subway(Integer money, String line) {
 		System.out.println("============================");
 		System.out.println("Subway 타기");
 		System.out.println("신문 보기");
